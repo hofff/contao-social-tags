d752c13d8b2b401760f538ff9ccfe68697394575
David Molineus <david.molineus@netzmacht.de>
2019-01-16 14:11:06 +0100

    Add missing translation.

--- src/Resources/contao/languages/de/tl_news.php
+++ src/Resources/contao/languages/de/tl_news.php
@@ -2,4 +2,7 @@
 
 declare(strict_types=1);
 
-$GLOBALS['TL_DCA']
+$GLOBALS['TL_LANG']['tl_news']['hofff_st_legend'] = 'Social Tags Metadaten';
+
+$GLOBALS['TL_LANG']['tl_news']['hofff_st'][0] = 'Social Tags definieren';
+$GLOBALS['TL_LANG']['tl_news']['hofff_st'][1] = 'Social Tags für diese News definieren';

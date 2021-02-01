15842e2918b151ae4b70639aec83ab84f0526f23
fritzmg <fmg@inspiredminds.at>
2021-02-01 12:27:14 +0000

    provide English translation

---
+++ src/Resources/contao/languages/en/hofff_st.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+$GLOBALS['TL_LANG']['hofff_st']['types']['general'] = 'General';
+$GLOBALS['TL_LANG']['hofff_st']['types']['website'] = 'Website';
+$GLOBALS['TL_LANG']['hofff_st']['types']['article'] = 'Article';
+$GLOBALS['TL_LANG']['hofff_st']['types']['profile'] = 'Profile';
+
+$GLOBALS['TL_LANG']['hofff_st']['types']['book']                = 'Book';
+$GLOBALS['TL_LANG']['hofff_st']['types']['music']               = 'Music';
+$GLOBALS['TL_LANG']['hofff_st']['types']['music.song']          = 'Song';
+$GLOBALS['TL_LANG']['hofff_st']['types']['music.album']         = 'Album';
+$GLOBALS['TL_LANG']['hofff_st']['types']['music.playlist']      = 'Playlist';
+$GLOBALS['TL_LANG']['hofff_st']['types']['music.radio_station'] = 'Radio station';
+
+$GLOBALS['TL_LANG']['hofff_st']['types']['video']         = 'Video';
+$GLOBALS['TL_LANG']['hofff_st']['types']['video.movie']   = 'Movie';
+$GLOBALS['TL_LANG']['hofff_st']['types']['video.episode'] = 'Episode';
+$GLOBALS['TL_LANG']['hofff_st']['types']['video.tv_show'] = 'TV Show';
+$GLOBALS['TL_LANG']['hofff_st']['types']['video.other']   = 'Other';
+$GLOBALS['TL_LANG']['hofff_st']['types']['custom']        = 'Custom type';
+
+$GLOBALS['TL_LANG']['hofff_st']['type'][0]                         = 'OpenGraph Type';
+$GLOBALS['TL_LANG']['hofff_st']['type'][1]                         = 'Type of the record.';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_og_title'][0]            = 'OpenGraph title';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_og_title'][1]            = 'This will fall back to the page title or name if empty.';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_og_site'][0]             = 'OpenGraph website';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_og_site'][1]             = 'This will fall back to the title or name of the root page if empty.';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_og_url'][0]              = 'OpenGraph canonical URL';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_og_url'][1]              = 'This will use the URL of the current page if empty.';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_og_image'][0]            = 'OpenGraph image';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_og_image'][1]            = 'Image to be used for the og:image meta tag.';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_og_description'][0]      = 'OpenGraph description';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_og_description'][1]      = 'This will use the meta description of the page if empty.';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_twitter_type'][0]        = 'Twitter Cards type';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_twitter_type'][1]        = '';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_twitter_title'][0]       = 'Twitter title';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_twitter_title'][1]       = 'This will fall back to the page title or name if empty.';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_twitter_image'][0]       = 'Twitter image';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_twitter_image'][1]       = 'Image to be used for the Twitter Card';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_twitter_description'][0] = 'Twitter description';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_twitter_description'][1] = 'This will use the meta description of the page if empty.';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_twitter_site'][0]        = 'Twitter site';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_twitter_site'][1]        = 'The Twitter @username for this website.';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_twitter_creator'][0]     = 'Twitter creator';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_twitter_creator'][1]     = 'The Twitter @username for this Twitter Card.';
+
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_twitter_types']['hofff_st_twitter_summary']             = 'Summary card';
+$GLOBALS['TL_LANG']['hofff_st']['hofff_st_twitter_types']['hofff_st_twitter_summary_large_image'] = 'Summary with large image';
---
+++ src/Resources/contao/languages/en/tl_calendar_events.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+$GLOBALS['TL_LANG']['tl_calendar_events']['hofff_st_legend'] = 'Social tags meta data';
+
+$GLOBALS['TL_LANG']['tl_calendar_events']['hofff_st'][0] = 'Define social tags';
+$GLOBALS['TL_LANG']['tl_calendar_events']['hofff_st'][1] = 'Define social tags for this event.';
